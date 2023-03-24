94d91bf89ad3396c8c7336860758ce24a1932683
yangweiyao <P@ssw0rd2023>
2023-03-24 16:51:52 +0800

    希尔排序-交换法

---
+++ src/main/java/com/yangweiyao/sort/random/ShellSort.java
@@ -0,0 +1,69 @@
+package com.yangweiyao.sort.random;
+
+import java.util.Arrays;
+
+/**
+ * @Author yangweiyao
+ * @CreateTime 2023-03-24 09:41
+ * @Description
+ **/
+public class ShellSort {
+
+    public static void main(String[] args) {
+
+        int[] nums = {45, 61, 92, 29, 17, 94, 83};
+
+        long start = System.currentTimeMillis();
+        exchangeShellSort(nums);
+        System.out.println("排序cost: " + (System.currentTimeMillis() - start));
+
+        System.out.println(Arrays.toString(nums));
+    }
+
+    /**
+     * 希尔排序-交换法：类似二分法思想
+     * [45, 61, 92, 29, 17, 94, 83]，pointer = length / 2 = 7 / 2 = 3
+     * @param nums 需要排序的数组
+     */
+    public static void exchangeShellSort(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+
+        // [45, 61, 92, 29, 17, 94, 83]
+        int[] copy = Arrays.copyOf(nums, nums.length);
+
+        boolean isExchange = false;
+
+        // 第一轮：pointer = length / 2 = 7 / 2 = 3
+        // 第二轮：pointer = 3 / 2 = 1
+        for(int i = nums.length / 2; i > 0; i /= 2) {
+            // 在第一轮分组下：遍历pointer右边[29, 17, 94, 83]
+            // 在第二轮分组下：遍历pointer右边[17, 92, 45, 61, 94, 83]
+            for(int j = i; j < nums.length; j++) {
+                // k = j - i与k -= i 主要是对应拿到pointer左边数组的值
+                for(int k = j - i; k >= 0; k -= i) {
+                    // pointer左边数组的值 与 pointer右边数组的值比较
+                    // 比如29与45 17与61 94与92 83与29
+                    // 最后一轮前后元素两两比较，把最大值往后挤
+                    if(nums[k + i] < nums[k]) {
+                        // 然后swag
+                        int temp = nums[k + i];
+                        nums[k + i] = nums[k];
+                        nums[k] = temp;
+                        isExchange = true;
+                    }
+                    if(!isExchange) {
+                        // 如果没有发生swag，直接退出
+                        break;
+                    }
+                    isExchange = false;
+                }
+            }
+        }
+    }
+
+    public static void insertShellSort(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        // TODO
+    }
+
+}
